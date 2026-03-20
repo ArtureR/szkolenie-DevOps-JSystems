52ae4af9332da1e6a108d4d191d4242dd18c2ff6
Artur <44artek2@gmail.com>
2026-03-20 14:10:23 +0100

    update

--- cwiczenia/TERRAFORM/01/main.tf
+++ cwiczenia/TERRAFORM/01/main.tf
@@ -1,6 +1,6 @@
 resource "digitalocean_droplet" "main" {
   name   = "artur-raj-github-actions-droplet" # dajcie sowje imie
-  region = "fra1"
-  size   = "s-1vcpu-1gb"
-  image  = "ubuntu-24-04-x64"
+           region = "fra1"
+  size                = "s-1vcpu-1gb"
+          image  = "ubuntu-24-04-x64"
 }
